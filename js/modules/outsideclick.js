dcaf680d2edfbe82b997db0fbc0b40e79f6a2aa1
milunaa <milenalunadosantos@gmail.com>
2023-04-06 14:31:29 -0300

    menu mobile 2

--- js/modules/outsideclick.js
+++ js/modules/outsideclick.js
@@ -1,18 +1,18 @@
-export default function outsideClick(element, events, callback){
+export default function outsideClick(element, events, callback) {
   const html = document.documentElement;
   const outside = 'data-outside';
 
-  if(!element.hasAttribute(outside)){
+  if(!element.hasAttribute(outside)) {
     events.forEach(userEvent => {
-      html.addEventListener(userEvent, handleOutsideClick);
-    })
+      setTimeout(() => html.addEventListener(userEvent, handleOutsideClick));
+    });
     element.setAttribute(outside, '');
   }
-  function handleOutsideClick(event){
-    if(!element.contains(event.target)){
+  function handleOutsideClick(event) {
+    if(!element.contains(event.target)) {
       element.removeAttribute(outside);
       events.forEach(userEvent => {
-        html.addEventListener(userEvent, handleOutsideClick);
+        html.removeEventListener(userEvent, handleOutsideClick);
       })
       callback();
     }
