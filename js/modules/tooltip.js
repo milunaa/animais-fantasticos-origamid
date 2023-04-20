02da08bc55a941e3a170347bde335aae08fb70c9
milunaa <milenalunadosantos@gmail.com>
2023-04-20 11:19:53 -0300

    atualizando

--- js/modules/tooltip.js
+++ js/modules/tooltip.js
@@ -1,39 +1,37 @@
-export default function initTooltip(){
+export default function initTooltip() {
   const tooltips = document.querySelectorAll('[data-tooltip]');
 
   tooltips.forEach((item) => {
     item.addEventListener('mouseover', onMouseOver);
   })
-
-  function onMouseOver(event){
-    const tooltipBox =  criarTooltipBox(this);
-    tooltipBox.style.top = event.pageY + 'px';
-    tooltipBox.style.left = event.pageX + 'px';
-
+  
+  function onMouseOver(event) {
+    const tooltipBox = criarTooltipBox(this);
+    
     onMouseMove.tooltipBox = tooltipBox;
     this.addEventListener('mousemove', onMouseMove);
-
+  
     onMouseLeave.tooltipBox = tooltipBox;
     onMouseLeave.element = this;
     this.addEventListener('mouseleave', onMouseLeave);
   }
-
+  
   const onMouseLeave = {
-    handleEvent(){
+    handleEvent() {
       this.tooltipBox.remove();
       this.element.removeEventListener('mouseleave', onMouseLeave);
       this.element.removeEventListener('mousemove', onMouseMove);
     }
   }
-
+  
   const onMouseMove = {
-    handleEvent(event){
+    handleEvent(event) {
       this.tooltipBox.style.top = event.pageY + 20 + 'px';
       this.tooltipBox.style.left = event.pageX + 20 + 'px';
     }
   }
-
-  function criarTooltipBox(element){
+  
+  function criarTooltipBox(element) {
     const tooltipBox = document.createElement('div');
     const text = element.getAttribute('aria-label');
     tooltipBox.classList.add('tooltip');
@@ -42,3 +40,4 @@ export default function initTooltip(){
     return tooltipBox;
   }
 }
+
